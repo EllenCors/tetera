16bac0e67b8679715658ad590e574f3039b2e3b9
EllenCors <soyellen_26@hotmail.com>
2017-04-09 15:04:47 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,9 @@
-/*
-
-	Copyright 2015 Ludwin Hernández
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    Crear cubo.
-*/
-
 #include <stdio.h>
 #include <GL/gl.h>
 #include <GL/glut.h>
 #include <math.h>
 
-//Definimos variables
+//variables
 double rotate_y=0;
 double rotate_x=0;
 double rotate_z=0;
@@ -150,7 +130,7 @@ int main(int argc, char* argv[])
     // Pasar el control de eventos a GLUT
     glutMainLoop();
 
-    // Regresar al sistema operativo
+    // Regresar 
     return 0;
 
 }
